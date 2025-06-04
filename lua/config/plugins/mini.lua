7ce52a2a7117a8f2e2622c2b4e7e613fa002fd6a
Santiago Mael Pandelo <santiagopandelo@frba.utn.edu.ar>
2025-06-04 18:41:51 -0300

    "Formatting"

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -2,23 +2,25 @@ return {
     "echasnovski/mini.nvim",
     version = false,
     config = function()
-        local comments = require("mini.comment")
-        local git = require("mini.git")
         local animate = require("mini.animate")
+        local comments = require("mini.comment")
         local cursorword = require("mini.cursorword")
+        local git = require("mini.git")
         local hipatterns = require("mini.hipatterns")
         local icons = require("mini.icons")
         local indentscope = require("mini.indentscope")
         local notify = require("mini.notify")
         local statusline = require("mini.statusline")
-        comments.setup({})
-        git.setup({})
+
         animate.setup({})
+        comments.setup({})
         cursorword.setup({})
+        git.setup({})
         icons.setup({})
         indentscope.setup({})
         notify.setup({})
         statusline.setup({})
+
         hipatterns.setup({
             highlighters = {
                 -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
